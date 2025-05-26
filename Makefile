37d4768d1d0b84821d02bc062a56e3570e619bdb
chenbo <asd1366464124@gmail.com>
2025-05-26 14:21:57 +0000

    generate ycsbbenchmark to memory device

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC=g++
 CFLAGS=-std=c++11 -g -Wall -pthread -I./
-LDFLAGS= -lpthread -ltbb -lhiredis
-SUBDIRS=core db redis
-SUBSRCS=$(wildcard core/*.cc) $(wildcard db/*.cc)
+LDFLAGS= -lpthread -ltbb # -lhiredis
+SUBDIRS=core # db redis
+SUBSRCS=$(wildcard core/*.cc) # $(wildcard db/*.cc)
 OBJECTS=$(SUBSRCS:.cc=.o)
 EXEC=ycsbc
 
--- ycsbc.cc
+++ ycsbc.cc
@@ -7,14 +7,16 @@
 //
 
 #include <cstring>
-#include <string>
-#include <iostream>
-#include <vector>
+#include <fstream>
 #include <future>
-#include "core/utils.h"
-#include "core/timer.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "core/client.h"
 #include "core/core_workload.h"
+#include "core/timer.h"
+#include "core/utils.h"
 #include "db/db_factory.h"
 
 using namespace std;
@@ -23,8 +25,83 @@ void UsageMessage(const char *command);
 bool StrStartWith(const char *str, const char *pre);
 string ParseCommandLine(int argc, const char *argv[], utils::Properties &props);
 
+struct MemDB : ycsbc::DB {
+  std::vector<ycsbc::DB::KVPair> data;
+  std::vector<ycsbc::Operation> ops;
+  MemDB(size_t size) {
+    data.reserve(size);
+    ops.reserve(size);
+  }
+
+  int Insert(const std::string &table, const std::string &key,
+             std::vector<ycsbc::DB::KVPair> &values) override {
+    string value;
+    // for (const auto &kv : values) {
+    //   value.append(kv.second);
+    // }
+    data.emplace_back(key, std::move(value));
+    ops.emplace_back(ycsbc::Operation::INSERT);
+
+    return ycsbc::DB::kOK;
+  }
+
+  int Read(const std::string &table, const std::string &key,
+           const std::vector<std::string> *fields,
+           std::vector<ycsbc::DB::KVPair> &result) override {
+    // Read logic for in-memory database
+    data.emplace_back(key, string());
+    ops.emplace_back(ycsbc::Operation::READ);
+
+    return ycsbc::DB::kOK;
+  }
+
+  int Update(const std::string &table, const std::string &key,
+             std::vector<ycsbc::DB::KVPair> &values) override {
+    // // Update logic for in-memory database
+    string value;
+    // for (const auto &kv : values) {
+    //   value.append(kv.second);
+    // }
+    data.emplace_back(key, value);
+    ops.emplace_back(ycsbc::Operation::UPDATE);
+    return ycsbc::DB::kOK;
+  }
+
+  int Scan(const std::string &table, const std::string &key, int record_count,
+           const std::vector<std::string> *fields,
+           std::vector<std::vector<ycsbc::DB::KVPair>> &result) override {
+    // Scan logic for in-memory database
+    data.emplace_back(key, to_string(record_count));
+    ops.emplace_back(ycsbc::Operation::SCAN);
+    return ycsbc::DB::kOK;
+  }
+  int Delete(const std::string &table, const std::string &key) override {
+    // Delete logic for in-memory database
+    data.emplace_back(key, string());
+    ops.emplace_back(ycsbc::Operation::DELETE);
+    return ycsbc::DB::kOK;
+  }
+  ~MemDB() override {
+    // dump to mem dev
+    ofstream fs("/mnt/memdb/db", ios::out | ios::trunc | ios::);
+    if (!fs.is_open()) {
+      cerr << "Failed to open memdb file for writing." << endl;
+      return;
+    }
+    for (int i = 0; i < data.size(); ++i) {
+      string op = ops[i] == ycsbc::Operation::INSERT   ? "INSERT"
+                  : ops[i] == ycsbc::Operation::READ   ? "READ"
+                  : ops[i] == ycsbc::Operation::UPDATE ? "UPDATE"
+                  : ops[i] == ycsbc::Operation::SCAN   ? "SCAN"
+                  : ops[i] == ycsbc::Operation::DELETE ? "DELETE"
+                                                       : "UNKNOWN";
+      fs << data[i].first << '\t' << op << '\n';
+    }
+  }
+};
+
 int DelegateClient(ycsbc::DB *db, ycsbc::CoreWorkload *wl, const int num_ops,
-    bool is_loading) {
+                   bool is_loading) {
   db->Init();
   ycsbc::Client client(*db, *wl);
   int oks = 0;
@@ -43,11 +120,14 @@ int main(const int argc, const char *argv[]) {
   utils::Properties props;
   string file_name = ParseCommandLine(argc, argv, props);
 
-  ycsbc::DB *db = ycsbc::DBFactory::CreateDB(props);
-  if (!db) {
-    cout << "Unknown database name " << props["dbname"] << endl;
-    exit(0);
-  }
+  // ycsbc::DB *db = ycsbc::DBFactory::CreateDB(props);
+  // if (!db) {
+  //   cout << "Unknown database name " << props["dbname"] << endl;
+  //   exit(0);
+  // }
+  int total_ops = stoi(props[ycsbc::CoreWorkload::RECORD_COUNT_PROPERTY]);
+  MemDB memdb(total_ops * 2);
+  ycsbc::DB *db = &memdb;
 
   ycsbc::CoreWorkload wl;
   wl.Init(props);
@@ -56,10 +136,9 @@ int main(const int argc, const char *argv[]) {
 
   // Loads data
   vector<future<int>> actual_ops;
-  int total_ops = stoi(props[ycsbc::CoreWorkload::RECORD_COUNT_PROPERTY]);
   for (int i = 0; i < num_threads; ++i) {
-    actual_ops.emplace_back(async(launch::async,
-        DelegateClient, db, &wl, total_ops / num_threads, true));
+    actual_ops.emplace_back(async(launch::async, DelegateClient, db, &wl,
+                                  total_ops / num_threads, true));
   }
   assert((int)actual_ops.size() == num_threads);
 
@@ -76,8 +155,8 @@ int main(const int argc, const char *argv[]) {
   utils::Timer<double> timer;
   timer.Start();
   for (int i = 0; i < num_threads; ++i) {
-    actual_ops.emplace_back(async(launch::async,
-        DelegateClient, db, &wl, total_ops / num_threads, false));
+    actual_ops.emplace_back(async(launch::async, DelegateClient, db, &wl,
+                                  total_ops / num_threads, false));
   }
   assert((int)actual_ops.size() == num_threads);
 
@@ -92,7 +171,8 @@ int main(const int argc, const char *argv[]) {
   cerr << total_ops / duration / 1000 << endl;
 }
 
-string ParseCommandLine(int argc, const char *argv[], utils::Properties &props) {
+string ParseCommandLine(int argc, const char *argv[],
+                        utils::Properties &props) {
   int argindex = 1;
   string filename;
   while (argindex < argc && StrStartWith(argv[argindex], "-")) {
@@ -170,12 +250,17 @@ void UsageMessage(const char *command) {
   cout << "Usage: " << command << " [options]" << endl;
   cout << "Options:" << endl;
   cout << "  -threads n: execute using n threads (default: 1)" << endl;
-  cout << "  -db dbname: specify the name of the DB to use (default: basic)" << endl;
-  cout << "  -P propertyfile: load properties from the given file. Multiple files can" << endl;
-  cout << "                   be specified, and will be processed in the order specified" << endl;
+  cout << "  -db dbname: specify the name of the DB to use (default: basic)"
+       << endl;
+  cout << "  -P propertyfile: load properties from the given file. Multiple "
+          "files can"
+       << endl;
+  cout << "                   be specified, and will be processed in the "
+          "order "
+          "specified"
+       << endl;
 }
 
 inline bool StrStartWith(const char *str, const char *pre) {
   return strncmp(str, pre, strlen(pre)) == 0;
 }
-
